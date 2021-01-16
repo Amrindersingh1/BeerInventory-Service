114390d2c1f76108fa05cead433d7b9552f9ead7
Amrinder singh <amrinders433@gmail.com>
2021-01-16 07:26:51 -0500

    COmpleted validation and allocation saga

---
+++ src/main/java/com/amrindersingh/beer/inventory/service/services/AllocationService.java
@@ -0,0 +1,10 @@
+package com.amrindersingh.beer.inventory.service.services;
+
+import com.brewery.model.BeerOrderDto;
+
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+
+    void deallocateOrder(BeerOrderDto beerOrderDto);
+}
